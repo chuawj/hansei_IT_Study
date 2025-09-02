a2d561b7f2874763b399f4c899b886d0b1ca7b31
최현준 <hanmy3541@gmail.com>
2025-09-02 18:13:11 +0900

    modified:   front/basket.html 	new file:   front/data.js 	modified:   front/register.html 	modified:   front/subject.html

---
+++ front/data.js
@@ -0,0 +1,24 @@
+const categoryData = [
+  { category:'신학과', depts:[{ name:'신학과', majors:['신학과'] }] },
+  { category:'사회과학부', depts:[{ name:'사회과학부', majors:['미디어영상광고학과','경영학과','관광경영학과'] }] },
+  { category:'공공서비스학부', depts:[{ name:'공공서비스학부', majors:['경찰행정학과','사회복지학과'] }] },
+  { category:'언어학부', depts:[{ name:'언어학부', majors:['영어학과','중국어학과'] }] },
+  { category:'IT학부', depts:[{ name:'IT학부', majors:['컴퓨터공학과','융합보안학과','IT학부'] }] },
+  { category:'간호학과', depts:[{ name:'간호학과', majors:['간호학과'] }] },
+  { category:'디자인학부', depts:[{ name:'디자인학부', majors:['시각정보디자인학과','실내건축디자인학과','섬유패션디자인학과'] }] },
+  { category:'예술학부', depts:[{ name:'예술학부', majors:['음악학과','공연예술학과'] }] },
+  { category:'자유전공학부', depts:[{ name:'자유전공학부', majors:['자유전공학부'] }] }
+];
+const subjects = [
+  { dept:'신학과', major:'신학과', year:1, name:'신약입문', code:'15866', type:'전공기초', credit:3, cap:30, prof:'정미배' },
+  { dept:'신학과', major:'신학과', year:1, name:'모세오경', code:'10582', type:'전공필수', credit:3, cap:30, prof:'안정해' },
+  { dept:'IT학부', major:'IT학부', year:1, name:'IT학부세미나', code:'25103', type:'전공기초', credit:3, cap:30, prof:'공삼공' },
+  { dept:'IT학부', major:'컴퓨터공학과', year:1, name:'웹프로그래밍', code:'19504', type:'전공필수', credit:3, cap:30, prof:'홍길동' },
+  { dept:'IT학부', major:'컴퓨터공학과', year:1, name:'자료구조', code:'19505', type:'전공필수', credit:3, cap:30, prof:'이몽룡' },
+  { dept:'IT학부', major:'융합보안학과', year:1, name:'정보보안개론', code:'19509', type:'전공기초', credit:3, cap:30, prof:'김보안' },
+  { dept:'사회과학부', major:'미디어영상광고학과', year:1, name:'광고학개론', code:'21001', type:'전공기초', credit:3, cap:30, prof:'박광고' },
+  { dept:'사회과학부', major:'경영학과', year:1, name:'경영학원론', code:'21002', type:'전공기초', credit:3, cap:30, prof:'최경영' },
+  { dept:'공공서비스학부', major:'경찰행정학과', year:1, name:'경찰학개론', code:'22001', type:'전공기초', credit:3, cap:30, prof:'이경찰' },
+  { dept:'언어학부', major:'영어학과', year:1, name:'영문법', code:'23001', type:'전공기초', credit:3, cap:30, prof:'정영문' },
+  { dept:'디자인학부', major:'시각정보디자인학과', year:1, name:'디자인기초', code:'24001', type:'전공기초', credit:3, cap:30, prof:'김디자인' }
+];
